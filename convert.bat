8a5f80feb71a97eaab760ccf57cf2dde421f3133
Dan Nixon <dan@dan-nixon.com>
2017-06-02 16:41:13 +0100

    Remove lap time

--- convert.bat
+++ convert.bat
@@ -2,7 +2,6 @@
 
 vocaloid_announcer_conv^
 	-s source/sources.json^
-	-t targets/laptime.json^
 	-t targets/multirotor.json^
-	-t targets/opentx_system.json^
+	-t targets/opentx21_system.json^
 	convert
