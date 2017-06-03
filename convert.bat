af15c7ff45a89a1899804490eb3d67f9cb0bb820
Dan Nixon <dan@dan-nixon.com>
2017-06-03 11:46:13 +0100

    Update style

--- convert.bat
+++ convert.bat
@@ -1,7 +1,8 @@
 @echo off
 
 vocaloid_announcer_conv^
+	--log-level WARN^
 	-s source/sources.json^
 	-t targets/multirotor.json^
-	-t targets/opentx21_system.json^
+	-t targets/opentx22_system.json^
 	convert
